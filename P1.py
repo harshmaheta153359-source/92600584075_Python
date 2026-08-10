ee7519fe5be923bff4d2d2ff9be5175d2b1a0af1
harshmaheta153359-source <harsh.maheta153359@marwadiuniversity.ac.in>
2026-08-10 10:18:05 +0530

    Delete P1.py

--- P1.py
+++
@@ -1,8 +0,0 @@
-# Write a program to demonstrate basic input and output operations in Python.
-
-name = input("Enter name : ")
-age  = int(input("Enter age : "))
-
-print("My name is ",name)
-print(f"I am {age} years old ")
-
