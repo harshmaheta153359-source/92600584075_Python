b95c439adbf7811b91ea251de4394bc161efb67e
harshmaheta153359-source <harsh.maheta153359@marwadiuniversity.ac.in>
2026-08-10 10:12:41 +0530

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,8 @@
+# Write a program to demonstrate basic input and output operations in Python.
+
+name = input("Enter name : ")
+age  = int(input("Enter age : "))
+
+print("My name is ",name)
+print(f"I am {age} years old ")
+
