e5e1ed87c02aa5e357a04f2f8ba84d772a12529c
harshmaheta153359-source <harsh.maheta153359@marwadiuniversity.ac.in>
2026-08-14 10:40:18 +0530

    Delete Unit1/P5.py

--- Unit1/P5.py
+++
@@ -1,8 +0,0 @@
-# list manipulation
-lst = [4,5,6,80,98]
-print("Before changing the list")
-print(lst)
-print("After appending")
-c = lst.append(1)
-print(lst)
-print("Indexing the list : ",lst[3])
