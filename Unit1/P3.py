667c7ab45c073a4aac51e30f4076f4cc285b043d
harshmaheta153359-source <harsh.maheta153359@marwadiuniversity.ac.in>
2026-08-17 10:17:35 +0530

    Add files via upload

---
+++ Unit1/P3.py
@@ -0,0 +1,20 @@
+
+name = "Harsh"          
+age = 21                
+marks = 56.5            
+is_correct = True       
+
+print(type(name))
+print(type(age))
+print(type(marks))
+print(type(is_correct))
+
+#type castings
+age = 21
+age_float = float(age)
+
+marks = 56.5
+marks_int = int(marks)
+
+print("Integer to Float:", age_float)
+print("Float to Integer:", marks_int)
