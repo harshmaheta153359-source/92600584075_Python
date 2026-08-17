2dc889a8bccc9a4986937c5d068ce86172933c50
harshmaheta153359-source <harsh.maheta153359@marwadiuniversity.ac.in>
2026-08-17 10:32:33 +0530

    Add files via upload

---
+++ Unit1/P3.py
@@ -0,0 +1,22 @@
+a = 10
+b = 5
+
+print("Arithmetic Operations:")
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("Modulus =", a % b)
+
+print("\nRelational Operations:")
+print("a == b :", a == b)
+print("a != b :", a != b)
+print("a > b  :", a > b)
+print("a < b  :", a < b)
+print("a >= b :", a >= b)
+print("a <= b :", a <= b)
+
+print("\nLogical Operations:")
+print("(a > b) and (a < 20) :", (a > b) and (a < 20))
+print("(a < b) or (a < 20)  :", (a < b) or (a < 20))
+print("not(a > b) :", not(a > b))
