fe68af0cee223ab9ab4034ca5bcaa5a4844ac286
harshmaheta153359-source <harsh.maheta153359@marwadiuniversity.ac.in>
2026-08-17 10:33:35 +0530

    Add files via upload

---
+++ Unit1/P4.py
@@ -0,0 +1,11 @@
+string = 'Python'
+
+print("String in uppercase:", string.upper())
+print("String in lowercase:", string.lower())
+print("String slicing:", string[0:2])
+print("String in title case:", string.title())
+print("Is the string alphabetic?:", string.isalpha())
+print("Count of 'string':", string.count("string"))
+print("Does the string end with 'n'?:", string.endswith("n"))
+
+
