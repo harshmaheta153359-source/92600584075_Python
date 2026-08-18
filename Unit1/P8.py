72898bbd469a1c7c5aa5194810bc05b61a874d69
harshmaheta153359-source <harsh.maheta153359@marwadiuniversity.ac.in>
2026-08-18 18:15:16 +0530

    Add files via upload

---
+++ Unit1/P8.py
@@ -0,0 +1,15 @@
+# Mutable and Immutable Objects
+
+# Mutable object - List
+list1 = [10, 20, 30]
+print("Original List:", list1)
+
+list1[0] = 100
+print("Modified List:", list1)
+
+
+# Immutable object - Tuple
+tuple1 = (10, 20, 30)
+print("Original Tuple:", tuple1)
+
+print("Tuple is immutable and cannot be changed.")
