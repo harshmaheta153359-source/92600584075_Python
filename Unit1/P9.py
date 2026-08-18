6c145f8183c1bfa9ab77e8e6b7c913a1d3fc5da3
harshmaheta153359-source <harsh.maheta153359@marwadiuniversity.ac.in>
2026-08-18 18:20:14 +0530

    Add files via upload

---
+++ Unit1/P9.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    print("Addition:", a + b)
+  
+def greet(name="Student"):
+    print("Hello", name)
+  
+def student(name, age):
+    print("Name:", name)
+    print("Age:", age)
+
+
+add(10, 20)
+
+greet()
+
+student(age=20, name="Harsh")
